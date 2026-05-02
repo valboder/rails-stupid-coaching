998215c11a6ca3cadcc92df908ccd09946e432dd
Valeska Bodero <vboderoa@fiec.espol.edu.ec>
2026-05-02 13:16:12 -0500

    change style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,14 +3,12 @@ class QuestionsController < ApplicationController
   end
   def answer
     @question = params[:question]
-    if @question.end_with?("?")
+    if @question.eql?("I am going to work")
+      @answer= "Great!"
+    elsif @question.end_with?("?")
       @answer= "Silly question, get dressed and go to work!"
-    elsif @question.eql?("I am going to work right now!")
-      @answer= ""
-    elsif @question.end_with?("!")
-      @answer= "I don't care, get dressed and go to work!"
     else
-      @answer= "Hello Coach!"
+      @answer= "I don't care, get dressed and go to work!"
     end
   end
 end
